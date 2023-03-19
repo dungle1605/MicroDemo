f50bf8c7bf2126ff15b16a6786f69ed9a4c8b69b
Le Tien Dung <dugle1605@gmail.com>
2023-03-19 12:37:42 +0700

    basket service

---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,33 @@
+using Basket.API.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
