6ef658e9332034b8b373d5acd61f5e6caa2af311
Le Tien Dung <tien.dung.le@dksh.com>
2023-04-14 17:08:34 +0700

    commit code

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,28 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+builder.WebHost
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+    })
+    .ConfigureLogging((hostingContext, loggingBuilder) =>
+    {
+        loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        loggingBuilder.AddConsole();
+        loggingBuilder.AddDebug();
+    });
+
+
+var app = builder.Build();
+await app.UseOcelot();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
